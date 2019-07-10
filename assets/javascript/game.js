df438590332dc5c57f7d1d37de31bcb5948ce755
dormin <williamsben91@gmail.com>
2019-07-10 17:07:14 -0600

    created advanced skeleton

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+$( document ).ready(function(){
+    
+    // Selects a random number to be shown at the start of the game
+    // Number should be should be between 19 - 120
+    //
+    $('#randomNumber').text(Random);
+    // Appending random number to the randomNumber id in the html doc
+    //
+  
+    // Setting up random numbers for each jewel
+    // Random number has to be between 1 - 12
+    // 
+    var userTotal= 0; 
+    var wins= 0;
+    var losses = 0;
+    //  Decaring variables for tallies
+  
+  //resets the game
+  
+  //adds the wins to the userTotal
+
+  //addes the losses to the userTotal
+
+  //sets up click for jewels
+   
+  //sets win/lose conditions
